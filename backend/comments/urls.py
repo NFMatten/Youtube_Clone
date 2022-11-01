d79a7ffd1eb0e78079cfe127615ab2a19aa9177f
Christian Beckham <urenacb22@gmail.com>
2022-11-01 15:21:09 -0500

    added url endpoint for retrieving comments and single comment

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_comments),
-    path('all/', views.get_all_comments),
-    path('<int:pk>/', views.user_comments)
+    # path('', views.user_comments),
+    path('<str:video_id>/comments/', views.video_comments),
+    path('<str:video_id>/comments/<int:pk>/', views.individual_comment),
+    # path('all/', views.get_all_comments),
+    # path('<int:pk>/', views.user_comments)
 ]
 
 # Accepts a value from the request's URL (The YouTube video id I am trying to get comments for)
