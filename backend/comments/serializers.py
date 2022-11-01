4e29fe001fc16b410ceb5e42a2cce2c686fa8a0b
Nikolaus Matten <nfmatten@gmail.com>
2022-11-01 10:49:25 -0600

    Created serializers for Comments and Replies apps

---
+++ backend/comments/serializers.py
@@ -0,0 +1,8 @@
+from select import select
+from rest_framework import serializers
+from .models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'video_id', 'text', 'likes', 'dislikes']
