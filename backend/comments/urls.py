0a4804c8f3cf1ccf5b21e13f9ca41d3252f7f246
Nikolaus Matten <nfmatten@gmail.com>
2022-11-01 12:53:05 -0600

    Added PUT functionality to Comments app views file

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.user_comments),
     path('all/', views.get_all_comments),
-    path('', views.user_comments)
+    path('<int:pk>/', views.user_comments)
 ]
 
 # Accepts a value from the request's URL (The YouTube video id I am trying to get comments for)
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Comment
 from .serializers import CommentSerializer
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -12,9 +13,9 @@ def get_all_comments(request):
     serializer = CommentSerializer(comments, many=True)
     return Response(serializer.data)
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT'])
 @permission_classes([IsAuthenticated])
-def user_comments(request):
+def user_comments(request, pk):
     print(
         'User ', f"{request.user.id} {request.user.email} {request.user.username}"
     )
@@ -24,7 +25,15 @@ def user_comments(request):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'GET':
         comments = Comment.objects.filter(user_id=request.user.id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        comment = get_object_or_404(Comment, pk=pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
