8feb73596fc48d0659f646506dc45b7bc894dd9d
Nikolaus Matten <nfmatten@gmail.com>
2022-11-01 10:59:34 -0600

    Created views functions for Comments and Replies apps

--- backend/replies/views.py
+++ backend/replies/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Reply
+from .serializers import ReplySerializer
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request):
+    comments = Reply.objects.all()
+    serializer = ReplySerializer(comments, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def user_comments(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}"
+    )
+    if request.method == 'POST':
+        serializer = ReplySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        comments = Reply.objects.filter(user_id=request.user.id)
+        serializer = ReplySerializer(comments, many=True)
+        return Response(serializer.data)
