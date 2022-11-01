3c3362d818c8dc2ca41f167e1a468be8c2b4964b
Nikolaus Matten <nfmatten@gmail.com>
2022-11-01 10:29:24 -0600

    Style: Corrected typo in Comment model

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from authentication.models import User
 
 # Create your models here.
-class Comments(models.Model):
+class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=30)
     text = models.CharField(max_length=255)
