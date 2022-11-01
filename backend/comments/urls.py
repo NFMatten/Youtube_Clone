1de77cab0c720ddd0a55dba9ca35b000ecb50fb1
Nikolaus Matten <nfmatten@gmail.com>
2022-11-01 15:13:32 -0600

    Created paths for CRUD methods in Comments App file

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.user_comments),
+    path('comments/', views.update_comments),
+    path('all/', views.get_all_comments),
     path('<str:video_id>/comments/', views.video_comments),
-    path('<str:video_id>/comments/<int:pk>/', views.individual_comment),
-    # path('all/', views.get_all_comments),
-    # path('<int:pk>/', views.user_comments)
+    path('<str:video_id>/comments/<int:pk>/', views.individual_comment)
 ]
-
-# Accepts a value from the request's URL (The YouTube video id I am trying to get comments for)
