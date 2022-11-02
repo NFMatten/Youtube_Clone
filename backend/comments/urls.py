92d0b0a66c73304000207b2c2a240c26d9f50c65
Nikolaus Matten <nfmatten@gmail.com>
2022-11-02 09:21:42 -0600

    Updated URL paths in Comment App URLs file + refactored Comment App's view file

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comments/', views.update_comments),
-    path('all/', views.get_all_comments),
-    path('<str:video_id>/comments/', views.video_comments),
-    path('<str:video_id>/comments/<int:pk>/', views.individual_comment)
+    path('', views.video_comments),
+    path('<int:comment_id>/', views.individual_comment)
 ]
 
 
-# remove video_id from path
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -6,47 +6,33 @@ from .models import Comment
 from .serializers import CommentSerializer
 from django.shortcuts import get_object_or_404
 
-
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def get_all_comments(request):
-    comments = Comment.objects.all()
-    serializer = CommentSerializer(comments, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
-def update_comments(request, pk):
-    print(
-        'User ', f"{request.user.id} {request.user.email} {request.user.username}"
-    )
-    if request.method == 'POST':
+def video_comments(request):
+    if request.method == 'GET':
+        video_id = request.query_params.get('video_id')
+        if video_id is not None:
+            comments = Comment.objects.filter(video_id=video_id)
+        else:
+            comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
         serializer = CommentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def video_comments(request, video_id):
-    if request.method == 'GET':
-        comments = Comment.objects.filter(video_id=video_id)
-        serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data)
-
-
 @api_view(['GET', 'PUT'])
 @permission_classes([IsAuthenticated])
-def individual_comment(request, video_id, pk):
+def individual_comment(request, comment_id):
     if request.method == 'GET':
-        comments = Comment.objects.filter(video_id=video_id, pk=pk)
+        comments = Comment.objects.filter(pk=comment_id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
     elif request.method == 'PUT':
-        comment = get_object_or_404(Comment, pk=pk)
+        comment = get_object_or_404(Comment, pk=comment_id)
         serializer = CommentSerializer(comment, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
