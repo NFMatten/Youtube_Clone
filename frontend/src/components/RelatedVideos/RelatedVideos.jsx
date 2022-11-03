d20e9165081c72c9d7789a6eb424bf492df7e9d9
Christian Beckham <urenacb22@gmail.com>
2022-11-03 16:56:53 -0500

    style: updated component styling

--- frontend/src/components/RelatedVideos/RelatedVideos.jsx
+++ frontend/src/components/RelatedVideos/RelatedVideos.jsx
@@ -5,30 +5,55 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+import { Link } from "react-router-dom";
 
 const RelatedVideos = (props) => {
-  const { relatedVideos } = props;
-  return (
-    <Grid>
-      {relatedVideos.map((video) => (
-        <Card sx={{ display: "flex" }} key={video.id.videoId}>
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <CardContent sx={{ flex: "1 0 auto" }}>
-              <Typography component="div" variant="h5">
-                {video.snippet.title}
-              </Typography>
-            </CardContent>
-          </Box>
-          <CardMedia
-            component="img"
-            sx={{ width: video.snippet.thumbnails.default.width }}
-            image={video.snippet.thumbnails.default.url}
-            alt="Live from space album cover"
-          />
-        </Card>
-      ))}
-    </Grid>
-  );
+	const { relatedVideos } = props;
+
+	return (
+		<Grid>
+			{relatedVideos.map((video) => (
+				<Card key={video.id.videoId} sx={{ marginBottom: "10px" }}>
+					<CardActionArea
+						sx={{ display: "flex" }}
+						component={Link}
+						to={`/video/${video.id.videoId}`}
+					>
+						<Box sx={{ display: "flex", flexDirection: "row" }}>
+							<CardContent sx={{ flex: "0" }} zeroMinWidth>
+								<div
+									style={{
+										overflow: "hidden",
+										textOverflow: "ellipsis",
+										width: "11rem",
+									}}
+								>
+									<Typography
+										noWrap
+										component="div"
+										variant="body1"
+										sx={{ overflow: "hidden" }}
+									>
+										{video.snippet.title}
+									</Typography>
+								</div>
+							</CardContent>
+							<CardMedia
+								component="img"
+								sx={{
+									width: video.snippet.thumbnails.default.width,
+									flex: "1",
+								}}
+								image={video.snippet.thumbnails.default.url}
+								alt="Live from space album cover"
+							/>
+						</Box>
+					</CardActionArea>
+				</Card>
+			))}
+		</Grid>
+	);
 };
 
 export default RelatedVideos;
