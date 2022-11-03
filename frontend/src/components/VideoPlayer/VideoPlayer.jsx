40089d9941ad47a974132e2364812030dcbcff08
Nikolaus Matten <nfmatten@gmail.com>
2022-11-03 13:44:33 -0600

    Created Video Player Component

---
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const VideoPlayer = (props) => {
+  const { videoId } = props;
+  return (
+    <iframe
+      id="ytplayer"
+      type="text/html"
+      width="640"
+      height="360"
+      src={`http://www.youtube.com/embed/${videoId}?autoplay=0`}
+      frameBorder="0"
+    ></iframe>
+  );
+};
+
+export default VideoPlayer;
