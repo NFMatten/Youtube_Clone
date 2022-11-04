62e40bd053ceaccc32c2ca4ad1bf283200bd8308
Christian Beckham <urenacb22@gmail.com>
2022-11-04 16:34:19 -0500

    feat: updated permission_classes decorator in the views.py file and added the comment_id FK field to the serializer

--- backend/replies/serializers.py
+++ backend/replies/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import Reply
 
+
 class ReplySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Reply
-        fields = ['id', 'user', 'comment', 'text']
+        fields = ['id', 'user', 'comment', 'text', 'comment_id']
+        depth = 1
+
+    comment_id = serializers.IntegerField(write_only=True)
--- backend/replies/views.py
+++ backend/replies/views.py
@@ -1,13 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import api_view, permission_classes
-from .models import Reply 
+from .models import Reply
 from .serializers import ReplySerializer
 
 
 @api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def comment_replies(request, comment_id):
     if request.method == 'GET':
         replies = Reply.objects.filter(comment__id=comment_id)
@@ -18,4 +18,4 @@ def comment_replies(request, comment_id):
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
