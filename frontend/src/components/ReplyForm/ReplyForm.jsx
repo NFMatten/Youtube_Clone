5bbaa42729596e2c43930a94f83d025d83a458ad
Christian Beckham <urenacb22@gmail.com>
2022-11-04 16:35:42 -0500

    feat: added ReplyForm component

---
+++ frontend/src/components/ReplyForm/ReplyForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Button, TextField } from "@mui/material";
+
+const ReplyForm = (props) => {
+	const { addReply } = props;
+	const [value, setValue] = useState("");
+
+	const handleChange = (e) => {
+		setValue(e.target.value);
+	};
+	const handleSubmit = () => {
+		const replyObj = { text: value };
+		addReply(replyObj);
+		setValue("");
+	};
+
+	return (
+		<div>
+			<TextField
+				id="outlined-multiline-flexible"
+				label="Reply"
+				multiline
+				maxRows={4}
+				value={value}
+				onChange={handleChange}
+				fullWidth
+				margin="normal"
+			/>
+			<Button variant="contained" type="submit" onClick={handleSubmit}>
+				Reply
+			</Button>
+		</div>
+	);
+};
+
+export default ReplyForm;
