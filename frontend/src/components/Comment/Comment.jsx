2a530b714b5f30ce26edcac3ba0a4a1dcbfea569
Nikolaus Matten <nfmatten@gmail.com>
2022-11-04 10:15:04 -0600

    feat: Updated props

--- frontend/src/components/Comment/Comment.jsx
+++ frontend/src/components/Comment/Comment.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 const Comment = (props) => {
-	const { user, text } = props;
-	return (
-		<div>
-			<h4>{user}</h4>
-			<h4>{text}</h4>
-		</div>
-	);
+  const {
+    user: { username },
+    text,
+  } = props;
+  return (
+    <div>
+      <h4>{username}</h4>
+      <h4>{text}</h4>
+    </div>
+  );
 };
 
 export default Comment;
