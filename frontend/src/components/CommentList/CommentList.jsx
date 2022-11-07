08d173d78089feb4295c5c185acefb3644a7d7fa
Nikolaus Matten <nfmatten@gmail.com>
2022-11-07 09:30:24 -0700

    updated props being passed to Comment component

--- frontend/src/components/CommentList/CommentList.jsx
+++ frontend/src/components/CommentList/CommentList.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Comment from "../Comment/Comment";
 
 const CommentList = (props) => {
-  const { comments } = props;
+  const { comments, getComments } = props;
 
   return (
     <div>
       {comments.map((comment) => (
-        <Comment {...comment} key={comment.id} />
+        <Comment {...comment} key={comment.id} getComments={getComments} />
       ))}
     </div>
   );
