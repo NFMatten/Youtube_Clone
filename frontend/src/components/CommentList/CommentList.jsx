a647aa48fddb21cf39fbd9c653e162ad3c5de1d0
Christian Beckham <urenacb22@gmail.com>
2022-11-07 16:38:29 -0600

    added conditional rendering to display if comments exist and added List component

--- frontend/src/components/CommentList/CommentList.jsx
+++ frontend/src/components/CommentList/CommentList.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import Comment from "../Comment/Comment";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
 
 const CommentList = (props) => {
-  const { comments, getComments } = props;
+	const { comments, getComments } = props;
 
-  return (
-    <div>
-      {comments.map((comment) => (
-        <Comment {...comment} key={comment.id} getComments={getComments} />
-      ))}
-    </div>
-  );
+	return (
+		<>
+			{comments.length > 0 ? (
+				<List>
+					{comments.map((comment) => (
+						<Comment {...comment} key={comment.id} getComments={getComments} />
+					))}
+				</List>
+			) : (
+				<Typography variant="h6">0 comments</Typography>
+			)}
+		</>
+	);
 };
 
 export default CommentList;
