2328412e876979a47eb8c1d0412d7c1eb33cd402
Christian Beckham <urenacb22@gmail.com>
2022-11-07 16:39:25 -0600

    updated JSX to include MUI components for styling and layout

--- frontend/src/components/Reply/Reply.jsx
+++ frontend/src/components/Reply/Reply.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
 
 const Reply = (props) => {
 	const { username, text } = props;
 
 	return (
-		<li>
-			{username} - {text}
-		</li>
+		<ListItem disablePadding disableGutters>
+			<ListItemAvatar sx={{ minWidth: "inherit", mr: 2 }}>
+				<Avatar
+					sx={{
+						width: 24,
+						height: 24,
+					}}
+					alt={username}
+				/>
+			</ListItemAvatar>
+			<ListItemText
+				primary={<Typography variant="subtitle1">{username}</Typography>}
+				secondary={<Typography variant="subtitle2">{text}</Typography>}
+			/>
+		</ListItem>
 	);
 };
 
