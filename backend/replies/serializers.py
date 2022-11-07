f753e08ef419ce76ea906bcaf43af0f98bdcd72a
Nikolaus Matten <nfmatten@gmail.com>
2022-11-07 14:58:54 -0700

    feat: Created nested serializer to help secure data

--- backend/replies/serializers.py
+++ backend/replies/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from .models import Reply
+from authentication.models import User
+from comments.models import Comment
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
 
 class ReplySerializer(serializers.ModelSerializer):
-
+    user = UserSerializer(many=False, read_only=True)
     class Meta:
         model = Reply
-        fields = ['id', 'user', 'comment', 'text', 'comment_id']
+        fields = ['id','user','comment','text', 'comment_id']
         depth = 1
-
     comment_id = serializers.IntegerField(write_only=True)
