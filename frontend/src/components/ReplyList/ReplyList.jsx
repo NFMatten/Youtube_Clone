a7eff54e80e1b3b4cf8a5f2b688b0c14e57bf597
Christian Beckham <urenacb22@gmail.com>
2022-11-07 16:40:27 -0600

    updated JSX to include MUI component for styling and layout

--- frontend/src/components/ReplyList/ReplyList.jsx
+++ frontend/src/components/ReplyList/ReplyList.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Reply from "../Reply/Reply";
+import List from "@mui/material/List";
 
 const ReplyList = (props) => {
-  const { replies } = props;
+	const { replies } = props;
 
-  return (
-    <div>
-      <ul>
-        {replies.map(({ id, user: { username }, text }) => (
-          <Reply key={id} username={username} text={text} />
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<List dense>
+			{replies.map(({ id, user: { username }, text }) => (
+				<Reply key={id} username={username} text={text} />
+			))}
+		</List>
+	);
 };
 
 export default ReplyList;
