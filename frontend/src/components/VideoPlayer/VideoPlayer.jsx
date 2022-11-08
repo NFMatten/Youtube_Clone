4516207f9c94eb6ca18e67bdda81411787875c18
Nikolaus Matten <nfmatten@gmail.com>
2022-11-08 15:56:45 -0700

    style: Updated video player size

--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const VideoPlayer = (props) => {
-	const { videoId } = props;
-	return (
-		<iframe
-			title={videoId}
-			id="ytplayer"
-			type="text/html"
-			width="640"
-			height="360"
-			src={`http://www.youtube.com/embed/${videoId}?autoplay=0`}
-			frameBorder="0"
-		></iframe>
-	);
+  const { videoId } = props;
+  return (
+    <iframe
+      title={videoId}
+      id="ytplayer"
+      type="text/html"
+      width="960"
+      height="540"
+      src={`http://www.youtube.com/embed/${videoId}?autoplay=0`}
+      frameBorder="0"
+    ></iframe>
+  );
 };
 
 export default VideoPlayer;
