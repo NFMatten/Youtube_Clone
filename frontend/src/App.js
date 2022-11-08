0596bae25b7e0b866580e50fee859dab414b1dff
Christian Beckham <urenacb22@gmail.com>
2022-11-08 14:26:17 -0600

    updated useEffect to fix rendering issue and updated getData function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // General Imports
 import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 
@@ -12,48 +12,49 @@ import VideoPage from "./pages/VideoPage/VideoPage";
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
-// import { items, relatedVideos } from "./sampledata";
 
 function App() {
-  const [videos, setVideos] = useState([]);
-  const [query, setQuery] = useState("");
+	const [videos, setVideos] = useState([]);
+	const [query, setQuery] = useState("");
+	const navigate = useNavigate();
 
-  useEffect(() => getData(), []);
+	useEffect(() => getData(), []);
 
-  useEffect(() => {
-    console.log(query, videos);
-    getData(query);
-  }, [query]);
+	useEffect(() => {
+		navigate("/");
+		setVideos([]);
+		getData(query);
+	}, [query]);
 
-  const getData = async (query) => {
-    try {
-      const response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/search`,
-        {
-          params: {
-            q: query,
-            key: process.env.REACT_APP_API_KEY,
-            part: "snippet",
-            maxResults: 12,
-          },
-        }
-      );
-      setVideos(response.data.items);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div>
-      <Navbar setQuery={setQuery} />
-      <Routes>
-        <Route exact path="/" element={<SearchPage videos={videos} />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/video/:videoId" element={<VideoPage videos={videos} />} />
-      </Routes>
-    </div>
-  );
+	const getData = async (query) => {
+		try {
+			const response = await axios.get(
+				`https://www.googleapis.com/youtube/v3/search`,
+				{
+					params: {
+						q: query,
+						key: process.env.REACT_APP_API_KEY,
+						part: "snippet",
+						maxResults: 12,
+					},
+				}
+			);
+			setVideos(response.data.items);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<div>
+			<Navbar setQuery={setQuery} />
+			<Routes>
+				<Route exact path="/" element={<SearchPage videos={videos} />} />
+				<Route path="/register" element={<RegisterPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/video/:videoId" element={<VideoPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
